6b0c4654d6bd1ede1c3b9d0244a352c3c052df11
edah-hub1 <cheruiyotedah@gmail.com>
2025-09-15 11:54:44 +0300

    add admins dashboard

--- src/app/dashboard/admin-dashboard/admin-dashboard.ts
+++ src/app/dashboard/admin-dashboard/admin-dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
+import { DashboardLayout } from '../dashboard-layout/dashboard-layout';
 
 @Component({
   selector: 'app-admin-dashboard',
+  imports: [DashboardLayout],
   standalone: true,
   templateUrl: './admin-dashboard.html',
   styleUrls: ['./admin-dashboard.css']
